31d5ac9426eb0623e54d061b5e68231b2bcd70d8
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-06-30 19:39:49 +0200

    add main component with picture

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import '../styles/Main.scss'
+
+
+function Main() {
+    return (
+        <div>
+            <img className='dino-picture' src='./zs9fTdh.gif' alt="dinosaurs"></img>
+        </div>
+    )
+}
+
+export default Main;
