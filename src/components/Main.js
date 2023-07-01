b08ceb15f6d423c4f23214b8edaca74a193d2118
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-01 17:45:47 +0200

    function for getting clicked coordinates

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,9 +2,21 @@ import '../styles/Main.scss'
 
 
 function Main() {
+
+    function grabClickCoordinates(event) {
+        console.log(window.innerWidth)
+        console.log(event.nativeEvent)
+        console.log(event.nativeEvent.offsetX)
+        console.log(event.nativeEvent.x)
+        const cordX = Math.floor(event.nativeEvent.offsetX / window.innerWidth * 100)
+        const cordY = Math.floor(event.nativeEvent.offsetY / window.innerHeight * 100)
+        console.log(cordX)
+        console.log(cordY)
+    }
+
     return (
         <div>
-            <img className='dino-picture' src='./zs9fTdh.gif' alt="dinosaurs"></img>
+            <img onClick={grabClickCoordinates} className='dino-picture' src='./zs9fTdh.gif' alt="dinosaurs"></img>
         </div>
     )
 }
