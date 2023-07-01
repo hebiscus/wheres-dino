65c9fed5928e19c1dfa000993408c47edcd55086
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-01 20:37:00 +0200

    targetBox on clickpops up at click coordinates

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,51 @@
 import '../styles/Main.scss'
+import styled from 'styled-components';
+import { useState } from 'react';
+
+const TargetBox = styled.div`
+    background-color: red;
+    display: ${props => props.$show ? "block" : "none"};
+    height: 30px;
+    width: 30px;
+    position: absolute;
+    left: ${props => props.$cord[0] + 'px'};
+    top: ${props => props.$cord[1] + 'px'}
+`
 
 
 function Main() {
+    const [clickPosition, setClickPosition] = useState([]);
+    const [showTargetBox, setShowTargetBox] = useState(false);
 
     function grabClickCoordinates(event) {
-        console.log(window.innerWidth)
         console.log(event.nativeEvent)
         console.log(event.nativeEvent.offsetX)
-        console.log(event.nativeEvent.x)
         const cordX = Math.floor(event.nativeEvent.offsetX / window.innerWidth * 100)
         const cordY = Math.floor(event.nativeEvent.offsetY / window.innerHeight * 100)
         console.log(cordX)
         console.log(cordY)
+        return [cordX, cordY]
+    }
+
+    function grabCordOnPage(event) {
+        const cordX = event.pageX;
+        const cordY = event.pageY;
+        return [cordX, cordY];
+    }
+
+    const toogleTarget = () => setShowTargetBox(!showTargetBox);
+
+    function targetBoxSwitch(event) {
+        toogleTarget();
+        const [cordX, cordY] = grabCordOnPage(event);
+        setClickPosition(
+            [cordX,cordY])
     }
 
     return (
         <div>
-            <img onClick={grabClickCoordinates} className='dino-picture' src='./zs9fTdh.gif' alt="dinosaurs"></img>
+            <img onClick={targetBoxSwitch} className='dino-picture' src='./zs9fTdh.gif' alt="dinosaurs"></img>
+            <TargetBox $show={showTargetBox} $cord={clickPosition} />
         </div>
     )
 }
