5a82fcbf2ff0c7e2a6b9bf5e711e44212e89d812
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-06 12:57:53 +0200

    add basic starting Screen and its logic switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './styles/App.scss';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
+import { useState } from 'react';
+import StartingScreen from './components/StartingScreen';
 
 function App() {
+  const [startingBox, setStartingBox] = useState(true);
+
+  const switchStartingBox = () => {
+    setStartingBox(false);
+  }
+
   return (
     <div className="App">
       <Navbar />
-      <Main />
+      {startingBox ? <StartingScreen switchStartingBox={switchStartingBox} /> : <Main />}
     </div>
   );
 }
---
+++ src/components/StartingScreen.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+import '../styles/App.scss'
+
+function StartingScreen({switchStartingBox}) {
+
+
+    return (
+        <div className='startBox'>
+            <h1>Welcome to Where's Dino!</h1>
+            <div className="start-instructions">
+                <p>It's a Where's Waldo type of game, where you have to find 4 characters.</p>
+                <p>But be careful, you're timed!</p>
+                <p> Coincidentally, you won't have to find any dinosaurs. Maybe because they're extinct...?</p>
+            </div>
+            <button onClick={switchStartingBox}>Start guessing</button>
+        </div>
+    )
+}
+
+export default StartingScreen;
