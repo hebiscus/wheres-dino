2854922a9827a8104002cc4f086fad70b1481605
hebiscus <107350293+hebiscus@users.noreply.github.com>
2023-07-09 13:40:37 +0200

    start screen content styling

--- src/components/StartingScreen.js
+++ src/components/StartingScreen.js
@@ -12,7 +12,7 @@ function StartingScreen({switchStartingBox, turnTimerOn}) {
         <div className='startBox'>
             <h1>Welcome to Where's Dino!</h1>
             <div className="start-instructions">
-                <p>It's a Where's Waldo type of game, where you have to find 4 characters.</p>
+                <p>This Where's Waldo type of game challenges you to find 4 characters.</p>
                 <p>But be careful, you're timed!</p>
                 <p> Coincidentally, you won't have to find any dinosaurs. Maybe because they're extinct...?</p>
             </div>
